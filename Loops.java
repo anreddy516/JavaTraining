f2bebf2e6fe914055e104cd77cee6cbe7ec39d29
anreddy516 <anreddy516@gmail.com>
2025-04-30 19:39:24 -0500

    Example for java loops

---
+++ Loops.java
@@ -0,0 +1,242 @@
+public class Loops {
+
+    public static void main(String[] args) {
+        exampleForWhileLoop();
+        exampleForDoWhile();
+        exampleForForLoop();
+//        printNumbersWithoutLoop();
+    }
+
+    public static void exampleForWhileLoop() {
+        int i = 1;
+
+        while (i <= 10) {
+            System.out.println(i);
+            i++;
+        }
+    }
+
+    public static void exampleForDoWhile() {
+        int i = 1;
+
+        do {
+            System.out.println(i);
+            i++;
+        } while ( i < 10);
+    }
+
+    public static void exampleForForLoop() {
+        for(int i = 1; i <= 10; i++) {
+            System.out.println(i);
+        }
+    }
+
+    public static void printNumbersWithoutLoop() {
+        int i = 1;
+        System.out.println(i);
+
+        i = i + 1;
+        System.out.println(i);
+
+        i = i + 1;
+        System.out.println(i);
+
+        i = i + 1;
+        System.out.println(i);
+
+        i = i + 1;
+        System.out.println(i);
+
+        i = i + 1;
+        System.out.println(i);
+
+        i = i + 1;
+        System.out.println(i);
+
+        i = i + 1;
+        System.out.println(i);
+
+        i = i + 1;
+        System.out.println(i);
+
+        i = i + 1;
+        System.out.println(i);
+
+        i = i + 1;
+        System.out.println(i);
+
+        i = i + 1;
+        System.out.println(i);
+
+        i = i + 1;
+        System.out.println(i);
+
+        i = i + 1;
+        System.out.println(i);
+
+        i = i + 1;
+        System.out.println(i);
+
+        i = i + 1;
+        System.out.println(i);
+
+        i = i + 1;
+        System.out.println(i);
+
+        i = i + 1;
+        System.out.println(i);
+
+        i = i + 1;
+        System.out.println(i);
+
+        i = i + 1;
+        System.out.println(i);
+
+        i = i + 1;
+        System.out.println(i);
+
+        i = i + 1;
+        System.out.println(i);
+
+        i = i + 1;
+        System.out.println(i);
+
+        i = i + 1;
+        System.out.println(i);
+
+        i = i + 1;
+        System.out.println(i);
+
+        i = i + 1;
+        System.out.println(i);
+
+        i = i + 1;
+        System.out.println(i);
+
+        i = i + 1;
+        System.out.println(i);
+
+        i = i + 1;
+        System.out.println(i);
+
+        i = i + 1;
+        System.out.println(i);
+
+        i = i + 1;
+        System.out.println(i);
+
+        i = i + 1;
+        System.out.println(i);
+
+        i = i + 1;
+        System.out.println(i);
+
+        i = i + 1;
+        System.out.println(i);
+
+        i = i + 1;
+        System.out.println(i);
+
+        i = i + 1;
+        System.out.println(i);
+
+        i = i + 1;
+        System.out.println(i);
+
+        i = i + 1;
+        System.out.println(i);
+
+        i = i + 1;
+        System.out.println(i);
+
+        i = i + 1;
+        System.out.println(i);
+
+        i = i + 1;
+        System.out.println(i);
+        i = i + 1;
+        System.out.println(i);
+
+        i = i + 1;
+        System.out.println(i);
+
+        i = i + 1;
+        System.out.println(i);
+
+        i = i + 1;
+        System.out.println(i);
+
+        i = i + 1;
+        System.out.println(i);
+        i = i + 1;
+        System.out.println(i);
+
+        i = i + 1;
+        System.out.println(i);
+
+        i = i + 1;
+        System.out.println(i);
+
+        i = i + 1;
+        System.out.println(i);
+
+        i = i + 1;
+        System.out.println(i);
+        i = i + 1;
+        System.out.println(i);
+
+        i = i + 1;
+        System.out.println(i);
+
+        i = i + 1;
+        System.out.println(i);
+
+        i = i + 1;
+        System.out.println(i);
+
+        i = i + 1;
+        System.out.println(i);
+        i = i + 1;
+        System.out.println(i);
+
+        i = i + 1;
+        System.out.println(i);
+
+        i = i + 1;
+        System.out.println(i);
+
+        i = i + 1;
+        System.out.println(i);
+
+        i = i + 1;
+        System.out.println(i);
+        i = i + 1;
+        System.out.println(i);
+
+        i = i + 1;
+        System.out.println(i);
+
+        i = i + 1;
+        System.out.println(i);
+
+        i = i + 1;
+        System.out.println(i);
+
+        i = i + 1;
+        System.out.println(i);
+        i = i + 1;
+        System.out.println(i);
+
+        i = i + 1;
+        System.out.println(i);
+
+        i = i + 1;
+        System.out.println(i);
+
+        i = i + 1;
+        System.out.println(i);
+
+        i = i + 1;
+        System.out.println(i);
+    }
+}
